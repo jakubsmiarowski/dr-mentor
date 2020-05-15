a37611f6a8e1760c21f039fbf1d7b9b11f3124ca
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2020-05-15 12:28:41 +0200

    add slick carousel

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var typed = new Typed('.type', {
     strings: [
-        'Business Coach',
-        'Marketing Specialist',
-        'Fucking awesome',
-        'Doctor Mentor'
+        'Diagnoza i szybka pomoc',
+        'Likwidacja bólu i leczenie przyczyny',
+        'Grupowo, indywidualnie, f2f i online',
+        'Doctor - Mentor ludzi korporacji'
     ],
     smartBackspace: true,
     typeSpeed: 100,
-    backSpeed: 40,
+    backSpeed: 20,
     backDelay: 1000,
     loop: true
+});
+
+
+$(document).ready(function() {
+    $('.slick').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        dots: true,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
